b81ebc6957e7faa7b0d832fac7bd46d0fb4617d5
Matthew Wierenga <matt.wierenga@outlook.com>
2023-04-03 16:33:31 +0200

    added a draw card function

---
+++ Elemental Battlegrounds/Assets/Scripts/DrawCards.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+
+     void Start()
+    {
+
+        
+    }
+
+    public void OnClick()
+    {
+        for (int i =0; i<5;i++)
+        {
+            GameObject playerCard = Instantiate(Card1, new Vector3(0, 0, 0), Quaternion.identity);
+            playerCard.transform.SetParent(PlayerArea.transform, false);
+
+        }
+
+
+    }
+
+}
